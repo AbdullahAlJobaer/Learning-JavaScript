e6dae4515000d5fa5a84027a3091ff8ab50579d2
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-12-15 20:58:21 +0600

    Enhancement #7: DOM Selectors for Multiple Elements

--- Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
+++ Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
@@ -44,7 +44,7 @@ scriptsArr.forEach(function(script) {
 
 
 console.log(val);
-*/
+
 
 
 // 023 DOM Selectors For Single Elements
@@ -87,3 +87,61 @@ document.querySelector('li:nth-child(4)').textContent = 'Hellow World';
 document.querySelector('li:nth-child(odd)').style.background = '#ccc';
 document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
+
+
+// 024 DOM Selectors for Multiple Elements
+
+// document.getElementsByClassName
+
+const items = document.getElementsByClassName('collection-item');
+
+console.log(items);
+console.log(items[0]);
+items[0].style.color = 'red';
+items[3].textContent = 'Hello';
+
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+console.log(listItems);
+
+
+// document.getElementsByTagName
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello';
+
+// Convert HTMl Collection into Array
+lis = Array.from(lis);
+
+lis.reverse();
+
+lis.forEach(function(li, index){
+    console.log(li.className);
+    li.textContent = `${index}: Hello`;
+});
+
+console.log(lis);
+*/
+
+// document.querySelectorAll
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+    item.textContent = `${index}: Hello`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even');
+
+liOdd.forEach(function(li, index){
+    li.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++){
+    liEven[i].style.background = '#f4f4f4';
+}
+
+console.log(items);
