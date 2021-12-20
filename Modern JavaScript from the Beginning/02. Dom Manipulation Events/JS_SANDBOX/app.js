b34bff45e5c616d15a6d42288541bbd5b4fadd3c
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-12-20 20:09:22 +0600

    Enhancement #7: Creating Elements

--- Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
+++ Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
@@ -145,7 +145,7 @@ for(let i = 0; i < liEven.length; i++){
 }
 
 console.log(items);
-*/
+
 
 
 
@@ -199,3 +199,36 @@ val = listItem.previousElementSibling;
 
 
 console.log(val);
+*/
+
+// 026 Creating Elements
+
+// Create element
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+// Add id
+li.id = 'New-item';
+
+// Add attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// Create new link element
+const link = document.createElement('a');
+// Add classes
+link.className = 'delete-item secondary-content';
+// Add icon HTML
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// Append link into li
+li.appendChild(link);
+
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
