284a830ac239e179b001abe30a8602d4b8093be1
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-12-28 19:38:51 +0600

    Enhancement #7: Event Listeners - The Event Object

--- Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
+++ Modern JavaScript from the Beginning/02. Dom Manipulation Events/JS_SANDBOX/app.js
@@ -231,7 +231,7 @@ li.appendChild(link);
 document.querySelector('ul.collection').appendChild(li);
 
 console.log(li);
-*/
+
 
 // 027 Removing Replacing Elements
 
@@ -287,4 +287,47 @@ link.removeAttribute('title');
 val = link;
 
 console.log(val);
+*/
 
+// 028 Event Listeners - The Event Object
+
+// document.querySelector('.clear-tasks').addEventListener('click', 
+// function(e){
+//     console.log('Hello World');
+
+//     //e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e){
+    //console.log('Clicked');
+
+    let val;
+    
+    val = e;
+
+    // Event target element
+    val = e.target;
+    val = e.target.id;
+    val = e.target.className;
+    val = e.target.classList;
+
+    //e.target.innerText = 'Hello';
+    
+    // Event type
+    val = e.type;
+    
+    // Timestamp
+    val = e.timeStamp;
+
+    // Co-ordinates event relative to the window
+    val = e.clientY;
+    val = e.clientX;
+
+    // Co-ordinates event relative to the element
+    val = e.offsetY;
+    val = e.offsetX;
+
+    console.log(val);
+}
