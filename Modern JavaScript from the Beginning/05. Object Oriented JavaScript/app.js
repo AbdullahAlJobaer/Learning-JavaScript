3f555264d2b8fc0eace4ccbaf33ea78e6f2f8aa0
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-02-21 21:03:18 +0600

    Enhancement #8: Practiced Built in Constructors

--- Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
+++ Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
@@ -1,3 +1,6 @@
+/*
+// 043 Constructors this keyword
+
 // Person constructor
 function Person(name, dob) {
     this.name = name;
@@ -17,3 +20,54 @@ function Person(name, dob) {
 
 const brad = new Person('Brad', '3-05-1995');
 console.log(brad.calculateAge());
+*/
+
+// 044 Built in Constructors
+
+// String
+
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+
+//name2.foo = 'bar';
+//console.log(name2);
+
+console.log(typeof name2);
+
+if(name2 === 'Jeff'){
+    console.log('YES');
+} else {
+    console.log('NO');
+}
+
+// Number
+const num1 = 5;
+const num2 = new Number(5);
+
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+// Function
+const getSum1 = function(x, y){
+    return x + y;
+}
+
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+// Object
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+
+console.log(john2);
+
+// Array
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re1);
+
