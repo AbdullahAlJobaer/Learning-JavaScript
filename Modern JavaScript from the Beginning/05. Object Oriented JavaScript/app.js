69286c9dfb75d19b61d7c0b0b902d92e8c45edc8
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-04-05 22:43:36 +0600

    Enhancement #8: Prototypes Explained

--- Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
+++ Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
@@ -20,7 +20,6 @@ function Person(name, dob) {
 
 const brad = new Person('Brad', '3-05-1995');
 console.log(brad.calculateAge());
-*/
 
 // 044 Built in Constructors
 
@@ -71,3 +70,56 @@ const re2 = new RegExp('\\w+');
 
 console.log(re1);
 
+*/
+
+
+// 045 Prototypes Explained
+
+//Object.prototype
+//Person.prototype
+
+// Person constructor
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+    // this.calculateAge = function(){
+    //     const diff = Date.now() - this.birthday.getTime();
+    //     const ageDate = new Date(diff);
+    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+    // }
+}
+
+// Calculate age
+Person.prototype.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+// Get Full Name
+Person.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`;
+}
+
+// Gets Married
+Person.prototype.getsMarried = function(newLastName){
+    this.lastName = newLastName;
+}
+
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+console.log(mary);
+
+console.log(john.calculateAge());
+
+console.log(mary.getFullName());
+
+mary.getsMarried('Smith');
+
+console.log(mary.getFullName());
+
+console.log(mary.hasOwnProperty('firstName'));
+
+console.log(mary.hasOwnProperty('getFullName'));
