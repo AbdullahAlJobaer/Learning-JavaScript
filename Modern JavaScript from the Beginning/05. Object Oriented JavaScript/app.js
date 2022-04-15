ede30056f22a13b62041e95c7728c7d876be8926
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-04-15 15:42:13 +0600

    Enhancement #8: Prototypical Inheritance

--- Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
+++ Modern JavaScript from the Beginning/05. Object Oriented JavaScript/app.js
@@ -70,7 +70,6 @@ const re2 = new RegExp('\\w+');
 
 console.log(re1);
 
-*/
 
 
 // 045 Prototypes Explained
@@ -123,3 +122,64 @@ console.log(mary.getFullName());
 console.log(mary.hasOwnProperty('firstName'));
 
 console.log(mary.hasOwnProperty('getFullName'));
+
+
+// 046 Prototypal Inheritance
+
+// Person constructor
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe');
+
+console.log(person1, greeting());
+*/
+
+// 046 Prototypical Inheritance
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;    
+}
+
+// Greeting
+Person.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe');
+
+console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+    Person.call(this, firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
+}
+
+// Imherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+}
+
+console.log(customer1.greeting());
