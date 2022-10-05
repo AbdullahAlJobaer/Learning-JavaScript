bed754f7dfca46652a153d05c81e1ddbb9948282
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-05 12:31:47 +0600

    Enhancement #9: Initial commit

---
+++ JS with Scrimba/Passenger Counter App/index.js
@@ -0,0 +1,97 @@
+//document.getElementById("count-el").innerText = 5
+
+// ctrl+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count  = firstBatch + secondBatch
+
+// console.log(count)
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge);
+
+
+// let count = 5
+
+// count = count + 1
+
+// console.log(count);
+
+
+// let bonusPoints = 50
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints * 2
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints / 4
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints * 3
+// console.log(bonusPoints);
+
+// function increment() {
+//     console.log("The button was clicked!");
+// }
+
+// function countdown() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(4);
+//     console.log(2);
+//     console.log(1);
+// }
+
+// countdown()
+// countdown()
+
+
+// function callNumber() {
+//     console.log(42);
+// }
+
+// callNumber()
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime);
+// }
+
+// logLapTime()
+
+// let lapsCompleted = 0
+
+// function incrementLap () {
+//     lapsCompleted = lapsCompleted + 1
+
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted);
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl);
+
+let count = 0;
+
+function increment() {
+    //console.log("Clicked!");
+    count = count + 1
+    countEl.innerText = count
+    //console.log(count);
+
+}
