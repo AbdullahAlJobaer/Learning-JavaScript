c878eed99593e3d87b12f484a682936a6bec5f32
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-12 20:56:30 +0600

    Enhancement #9: Strings vs Numbers

--- JS with Scrimba/Passenger Counter App/index.js
+++ JS with Scrimba/Passenger Counter App/index.js
@@ -130,7 +130,7 @@ let messageToUser = message + ", " + userName
 console.log(messageToUser);
 */
 
-
+/*
 //S1L20 Log a greeting to the console (6 OCT)
 
 let name = "Jobaer"
@@ -138,3 +138,25 @@ let greeting = "Hi, my name is "
 let myGreeting = greeting + name
 
 console.log(myGreeting);
+*/
+
+//S1L21 Strings vs Numbers
+
+
+let name = "Jobaer"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+
+//console.log(myGreeting);
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+
+console.log(totalPoints);
+
+console.log(4 + 5); // 9
+console.log("2" + "4"); // "24"
+console.log("5" + 1); // "51"
+console.log(100 + "100"); // "100100"
