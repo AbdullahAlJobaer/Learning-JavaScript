901eee1852d980a5aa2604e06d5e2aaef672075c
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-12 21:06:38 +0600

    Enhancement #9: Improved the message with string concatenation

--- JS with Scrimba/Passenger Counter App/Rendering a welcome message/index.js
+++ JS with Scrimba/Passenger Counter App/Rendering a welcome message/index.js
@@ -1,3 +1,4 @@
+/*
 //S1L22 Render a welcome message
 
 let welcomeEl = document.getElementById("welcome-el")
@@ -7,3 +8,16 @@ let greeting = "Welcome back, "
 
 
 welcomeEl.innerText = greeting + name
+*/
+
+// S1L23 Improve the message with string concatenation
+
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Jobaer"
+let greeting = "Welcome back, "
+
+
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText += " 👋"
