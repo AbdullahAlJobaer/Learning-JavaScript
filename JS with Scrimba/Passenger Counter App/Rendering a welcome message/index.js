2db876eec267c9c51ecd18f6635a8725c72a9c45
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-12 21:05:29 +0600

    Enhancement #9: Rendered a welcome message

---
+++ JS with Scrimba/Passenger Counter App/Rendering a welcome message/index.js
@@ -0,0 +1,9 @@
+//S1L22 Render a welcome message
+
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Jobaer"
+let greeting = "Welcome back, "
+
+
+welcomeEl.innerText = greeting + name
