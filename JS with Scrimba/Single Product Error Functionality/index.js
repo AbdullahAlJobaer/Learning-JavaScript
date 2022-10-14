91211219176f238818ac04a002fee7900064ae59
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-14 15:18:02 +0600

    Enhancement #10: Strings and Numbers

--- JS with Scrimba/Single Product Error Functionality/index.js
+++ JS with Scrimba/Single Product Error Functionality/index.js
@@ -21,6 +21,7 @@ function myGreeting() {
 myGreeting()
 */
 
+/*
 //S2L3 Incrementing and decrementing
 
 let myPoints = 3
@@ -39,3 +40,13 @@ add3Points()
 remove1Point()
 remove1Point()
 console.log(myPoints);
+*/
+
+//S2L4 Strings and numbers
+
+console.log("2" + 2);   // "22"
+console.log(11 + 7);   // 18
+console.log(6 + "5");   // "65"
+console.log("My points: " + 5 + 9);   // "My points: 59"
+console.log(2 + 2);   // 4
+console.log("11" + "14");   // "1114"
