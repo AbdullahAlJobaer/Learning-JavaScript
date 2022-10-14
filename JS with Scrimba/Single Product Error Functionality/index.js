4e49fc02c96e9cd2c05740cfbd5bbec7291c1ca7
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-14 15:16:20 +0600

    Enhancement #10: Concatenate two strings in a function

--- JS with Scrimba/Single Product Error Functionality/index.js
+++ JS with Scrimba/Single Product Error Functionality/index.js
@@ -1,3 +1,4 @@
+/*
 //S2L2 Variable practice
 
 let firstName = "Abdullah"
@@ -5,3 +6,15 @@ let lastName = "Jobaer"
 let fullName = firstName + " " + lastName
 
 console.log(fullName);
+*/
+
+//S2L2 Concatenate two strings in a function
+
+let name  = "Linda"
+let greeting = "Hi there"
+
+function myGreeting() {
+    console.log(greeting + ", " + name + "!");
+}
+
+myGreeting()
