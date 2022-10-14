262f6e40d4f0adade18a090993dc8776d1a396f4
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-10-14 15:17:18 +0600

    Enhancement #10: Incrementing and decrementing

--- JS with Scrimba/Single Product Error Functionality/index.js
+++ JS with Scrimba/Single Product Error Functionality/index.js
@@ -8,6 +8,7 @@ let fullName = firstName + " " + lastName
 console.log(fullName);
 */
 
+/*
 //S2L2 Concatenate two strings in a function
 
 let name  = "Linda"
@@ -18,3 +19,23 @@ function myGreeting() {
 }
 
 myGreeting()
+*/
+
+//S2L3 Incrementing and decrementing
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Point() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+console.log(myPoints);
