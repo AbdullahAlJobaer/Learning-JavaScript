7ccf413e4ef595d87a28f42a897f38529dd1d931
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-08 14:25:17 +0600

    Enhancement #11: Created NEW CARD functionality
    Add to the sum when NEW CARD is clicked.

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -264,6 +264,7 @@ function startGame() {
 }
 */
 
+/*
 //S3L17 New Card Button
 
 let firstCard = 10
@@ -296,4 +297,43 @@ function startGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!");
+}
+*/
+
+//S3L18 Add to the sum when newCard is clicked
+
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame() {  
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 7
+    sum += card
+
+    startGame()
 }
