b168bf827e8751528ccdf581b92676d0be57acdd
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-08 12:41:12 +0600

    Enhancement #11: Display the Cards

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -200,6 +200,7 @@ function startGame() {
 }
 */
 
+/*
 //S3L15 Display the sum
 
 let firstCard = 10
@@ -227,4 +228,37 @@ function startGame() {
 
     messageEl.textContent = message
     sumEl.textContent = "Sum: " + sum
+}
+
+*/
+
+
+//S3L16 Display the cards
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame() {  
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 }
