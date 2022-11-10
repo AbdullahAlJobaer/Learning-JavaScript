f7512d93fc0729f77e3553bd3a8c271e51e8f8d8
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-10 17:12:29 +0600

    Enhancement #11: Renamed the START GAME function

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -300,6 +300,7 @@ function newCard() {
 }
 */
 
+/*
 //S3L18 Add to the sum when newCard is clicked
 
 let firstCard = 10
@@ -336,4 +337,45 @@ function newCard() {
     sum += card
 
     startGame()
+}
+*/
+
+//S3L19 Rename the startGame function
+
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {  
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 7
+    sum += card
+    renderGame()
 }
