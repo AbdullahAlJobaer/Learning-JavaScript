006eefdd68d20cbfd864bb0a93e5f6c8bf1df9c6
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-11 16:29:08 +0600

    Enhancement #11: Array Indexes

--- JS with Scrimba/Blackjack Game/Intro to Arrays/index.js
+++ JS with Scrimba/Blackjack Game/Intro to Arrays/index.js
@@ -1,3 +1,4 @@
+/*
 //S3L21 Intro to Arrays
 // Arrays - ordered lists of items
 
@@ -11,8 +12,30 @@ let featuredPosts = [
 // The items of the array should be strings
 
 let myExperience = [
-    "Analyst - Data Processing at Quantanite",
-    "Associate - Data Processing at Quantanite"
+    "Ceo at Scrimba",
+    "Frontend developer at Xeneta",
+    "People counter for Norstat"
 ]
 
-console.log(myExperience);
+*/
+
+//S3L22 Array Indexes
+
+let featuredPosts = [
+    "Check out my Netflix clone", 
+    "Here's the code for my project", 
+    "I've just relaunched my portfolio"
+]
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let myExperience = [
+    "Ceo at Scrimba",
+    "Frontend developer at Xeneta",
+    "People counter for Norstat"
+]
+
+console.log(myExperience[1]);
+console.log(myExperience[2]);
+console.log(myExperience[0]);
