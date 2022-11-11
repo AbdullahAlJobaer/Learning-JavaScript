2253c7c7a70e0496a37ab02bfd39f2eb70d0531f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-11 14:54:06 +0600

    Enhancement #11: Intro to Arrays

---
+++ JS with Scrimba/Blackjack Game/Intro to Arrays/index.js
@@ -0,0 +1,18 @@
+//S3L21 Intro to Arrays
+// Arrays - ordered lists of items
+
+let featuredPosts = [
+    "Check out my Netflix clone", 
+    "Here's the code for my project", 
+    "I've just relaunched my portfolio"
+]
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let myExperience = [
+    "Analyst - Data Processing at Quantanite",
+    "Associate - Data Processing at Quantanite"
+]
+
+console.log(myExperience);
