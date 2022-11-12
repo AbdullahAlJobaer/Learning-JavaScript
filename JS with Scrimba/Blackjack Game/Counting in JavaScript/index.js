715803fe8b9f9a81479c6a65e75af3585b589b3f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-12 14:42:56 +0600

    Enhancement #3: Writing the first loop

---
+++ JS with Scrimba/Blackjack Game/Counting in JavaScript/index.js
@@ -0,0 +1,18 @@
+/*
+//S3L27 Counting in JavaScript
+
+for (let count = 10; count < 21; count +=1) {
+    
+    console.log(count);
+
+}
+*/
+
+//S3L28 Write your first loop
+
+// Create a for loop that counts 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let i = 10; i < 101; i +=10) {
+    console.log(i);  
+}
