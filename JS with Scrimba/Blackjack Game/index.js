2e0ae9d171b28cf612fbda62e2a3c94fcac1dced
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-12 21:34:58 +0600

    Enhancement #11: Used a loop to render cards

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -423,7 +423,7 @@ function newCard() {
     renderGame()
 }
 */
-
+/*
 //S3L26 Push a new card to the array
 
 let firstCard = 10
@@ -466,3 +466,52 @@ function newCard() {
     console.log(cards);
     renderGame()
 }
+*/
+
+//S3L32 Use a loop to render cards
+
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 6
+    sum += card
+    cards.push(card)
+    console.log(cards);
+    renderGame()
+} 
