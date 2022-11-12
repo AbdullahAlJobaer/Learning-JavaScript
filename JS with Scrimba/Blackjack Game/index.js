5506077f38c823993bd5e8cf2aedc379a135dd62
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-12 14:25:59 +0600

    Enhancement #11: Added new card to the array

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -381,7 +381,7 @@ function newCard() {
     renderGame()
 }
 */
-
+/*
 //S3L20 Solving our cards problem with an array
 
 let firstCard = 10
@@ -420,5 +420,49 @@ function newCard() {
     console.log("Drawing a new card from the deck!");
     let card = 7
     sum += card
+    renderGame()
+}
+*/
+
+//S3L26 Push a new card to the array
+
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {  
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 6
+    sum += card
+    cards.push(card)
+    console.log(cards);
     renderGame()
 }
