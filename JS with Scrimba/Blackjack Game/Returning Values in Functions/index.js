a67705973bb9cc4a9ae33d405578180b98501916
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-13 12:23:44 +0600

    Enhancement #11: Moderated random card function
    Modified the getRandomCard function to get random numbers between 1 to 13

--- JS with Scrimba/Blackjack Game/Returning Values in Functions/index.js
+++ JS with Scrimba/Blackjack Game/Returning Values in Functions/index.js
@@ -1,3 +1,4 @@
+/*
 //S3L34 Returning values in functions
 
 let player1Time = 102
@@ -23,3 +24,85 @@ function getTotalRaceTime() {
 let sumRaceTime = getTotalRaceTime()
 
 console.log(sumRaceTime);
+*/
+
+/*
+//S3L36 Generating random numbers with Math.random()
+
+let randomNumber = Math.random()
+
+console.log(randomNumber)
+
+/*
+
+What does Matg.random() do?
+
+Answer: It generates random decimal number between 0 to 1 (not inclusive of 1)
+
+*/
+/*
+//S3L37 Math.random() *6
+
+let randomNumber = Math.random() * 6
+
+console.log(randomNumber);
+
+/*
+
+In which range will our randomNumber be now?
+
+From: 0.00
+To: 5.99
+
+Math.random() : 0.000 - 0.999
+Math.random()*6 : 0.000 - 5.999
+
+*/
+
+/*
+//S3L38 Flooring the number with Math.random()
+
+let flooredNumber = Math.floor(12.99)
+
+console.log(flooredNumber);
+
+/*
+
+What does Math.floor() do to positive numbers?
+
+Answer: It removes the decimals
+
+*/
+
+/*
+//S3L39 Using Math.random() and Math.floor() to create a dice
+
+let randomNumber = Math.floor(Math.random() * 6)
+
+console.log(randomNumber);
+
+/*
+
+Write down all the possible values random number can hold now!
+
+Ans: 0, 1, 2, 3, 4, 5
+
+Math.random() : 0.000 - 0.999
+Math.random()*6 : 0.000 - 5.999
+Math.floor(Math.random() * 6) : 0 - 5
+
+*/
+
+
+//S3L40 Completing our dice function
+
+// Create a function, rollDice(), that returns a random number between 1 and 6
+
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    return randomNumber
+}
+
+let rollTheDice = rollDice()
+
+console.log(rollTheDice);
--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -518,6 +518,7 @@ function newCard() {
 } 
 */
 
+/*
 //S3L35 Using a function to set the card values
 
 let firstCard = getRandomCard()
@@ -569,4 +570,58 @@ function newCard() {
     cards.push(card)
     console.log(cards);
     renderGame()
-} 
+}
+*/
+
+
+//S3L41 Completing our dice function
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function getRandomCard() {
+    return Math.floor(Math.random() * 13) + 1   // 1 to 13
+}
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards);
+    renderGame()
+}
