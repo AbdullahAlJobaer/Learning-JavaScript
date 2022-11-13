c81dfb3a5accb68ad113e447d2c4a4c945e3bc8a
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-14 01:15:51 +0600

    Enhancement #11: Object Sneak Peek

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -754,7 +754,7 @@ function newCard() {
 }
 */
 
-
+/*
 //S3L44 Our new card feature is broken
 
 let cards = []
@@ -817,4 +817,147 @@ function newCard() {
     cards.push(card)
     console.log(cards);
     renderGame()
+}
+*/
+
+/*
+//S3L48 Only trigger newCard() if you're allowed to
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+console.log(cards);
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;   // 1 to 13
+
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards);
+        renderGame()
+    }
+}
+*/
+
+//S3L49 Object sneak peek
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+let player = {
+    name : "Jobaer",
+    chips : 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;   // 1 to 13
+
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards);
+        renderGame()
+    }
 }
