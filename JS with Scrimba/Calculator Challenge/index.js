e956decb5133ff815fb036afb155e35acfb5e732
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-14 14:30:15 +0600

    Enhancement #12: Created addition functionality

--- JS with Scrimba/Calculator Challenge/index.js
+++ JS with Scrimba/Calculator Challenge/index.js
@@ -5,3 +5,9 @@ document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
 let sumEl = document.getElementById("sum-el")
+
+function add() {
+    //console.log("Add function selected");
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
