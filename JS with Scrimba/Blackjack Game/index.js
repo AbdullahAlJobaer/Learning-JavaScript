737581548ba284792da551a82ed024be97f85048
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-14 14:04:15 +0600

    Enhancement #11: Finished building the game

--- JS with Scrimba/Blackjack Game/index.js
+++ JS with Scrimba/Blackjack Game/index.js
@@ -888,6 +888,7 @@ function newCard() {
     }
 }
 */
+/*
 
 //S3L49 Object sneak peek
 
@@ -961,3 +962,166 @@ function newCard() {
         renderGame()
     }
 }
+*/
+/*
+
+//S3L52 Use an object to store player data
+
+let player = {
+    name : "Jobaer",
+    chips : 200
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;   // 1 to 13
+
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards);
+        renderGame()
+    }
+}
+*/
+
+//S3L53 Methods on object
+
+let player = {
+    name : "Jobaer",
+    chips : 200
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;   // 1 to 13
+
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+       
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards);
+        renderGame()
+    }
+}
+
+/*
+
+RECAP
+- arrays
+- objects
+- booleans
+- if... else statements
+- comparison operators
+- logical operators
+- for loops
+- math object
+- return statements
+
+
+*/
