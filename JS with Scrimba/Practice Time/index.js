92b5fb884c2ac56b369dec4fa581d8912ececc60
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-14 21:38:21 +0600

    Enhancement #13: If else practice

--- JS with Scrimba/Practice Time/index.js
+++ JS with Scrimba/Practice Time/index.js
@@ -1,3 +1,4 @@
+/*
 //S4L1 Objects and functions
 
 // Create a person object that contains three keys: name, age, and county.
@@ -20,3 +21,31 @@ function logData() {
 }
 
 logData()
+
+*/
+
+//S4L2 if else
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+
+let age = 20
+
+if (age < 6) {
+    console.log("Free!");
+} else if (age < 18) {
+    console.log("Child discount!");
+} else if (age < 27) {
+    console.log("Student discount");
+} else if (age < 67) {
+    console.log("Full price!");
+} else {
+    console.log("Senior citizen discount!");
+}
