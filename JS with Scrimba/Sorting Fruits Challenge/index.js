fc45532795c8eec740ce53496d0b8c93f2e99b7f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-15 13:48:01 +0600

    Enhancement #14: Initial commit with html and CSS

---
+++ JS with Scrimba/Sorting Fruits Challenge/index.js
@@ -0,0 +1,5 @@
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
