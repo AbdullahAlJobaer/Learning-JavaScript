cb813cec68846f24b4001efe558a91c263b46f83
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-22 23:53:06 +0600

    Enhancement #15: Updated JS with challenge task

---
+++ JS with Scrimba/Build a Chrome Extention/Detour/index.js
@@ -0,0 +1,5 @@
+//S5L6 Write your first addEventListener()
+
+// 1. Grab the box from the DOM and store it in a variable
+// 2. Add a click event listener to the box 
+// 3. Log out "I want to open the box!" when it's clicked
