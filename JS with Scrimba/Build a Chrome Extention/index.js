f1fc0c771f4062863d502554aa2454874a9b5137
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-28 14:21:30 +0600

    Enhancement #15: My turn to refactor

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -1,4 +1,15 @@
+/*
 //S5L5 Refactor to addEventListener
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked!");
+})
+*/
+
+
+//S5L7 Your turn to refactor
 
 let inputBtn = document.getElementById("input-btn")
 
