c0f4a01f58fc63350df4a4aeb449636424953ffa
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-28 14:28:54 +0600

    Enhancement #15: Created myLeads array and inputEl

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -8,10 +8,27 @@ inputBtn.addEventListener("click", function(){
 })
 */
 
-
+/*
 //S5L7 Your turn to refactor
 
 let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked!");
+})
+
+*/
+
+
+//S5L8 Create the myLeads array and inputEl
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
     console.log("Button Clicked!");
