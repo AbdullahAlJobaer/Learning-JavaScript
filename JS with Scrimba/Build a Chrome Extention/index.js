8cd461c805a3e8e9ac9b8be8bce68fecd95c540e
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-11-28 23:57:19 +0600

    Enhancement #15: Rendered leads in unordered list

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -61,6 +61,7 @@ inputBtn.addEventListener("click", function(){
 })
 */
 
+/*
 //S5L12 Use a for loop to logout leads
 
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
@@ -77,3 +78,27 @@ inputBtn.addEventListener("click", function(){
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i]);
 }
+
+*/
+
+
+//S5L14 Render the leads in the unordered list
+
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+})
+
+// Log out the items in the myLeads array using a for loop
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i] + " "
+
+}
+
+
