dc14ecb8310655add94f58fda24f2644c7e1423b
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-06 22:13:12 +0600

    Enhancement #15: Template strings on multiple lines

--- JS with Scrimba/Build a Chrome Extention/Detour/index.js
+++ JS with Scrimba/Build a Chrome Extention/Detour/index.js
@@ -1,3 +1,4 @@
+/*
 //S5L25 Write your first template string
 
 // template strings/literals
@@ -8,3 +9,35 @@ const recipient = "James"
 const email = `Hey ${recipient}! How is it going? Cheers Per`
 
 console.log(email)
+*/
+
+/*
+//S5L26 Make the template string even more dynamic
+
+// template strings/literals
+
+const recipient = "James"
+const sender = "Per"
+
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+
+console.log(email)
+*/
+
+//S5L27 Template strings on multiple lines
+
+// template strings/literals
+
+const recipient = "James"
+const sender = "Per"
+
+const email = `
+Hey ${recipient}! 
+
+How is it going?
+
+Cheers ${sender}
+`
+
+console.log(email)
+
