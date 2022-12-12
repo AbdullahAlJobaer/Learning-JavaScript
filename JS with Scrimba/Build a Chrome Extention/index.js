6d74974cb8ec951bc0a4e101facd3994dcb58565
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-12 22:29:55 +0600

    Enhancement #15: Made the delete button work

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -367,6 +367,7 @@ function renderLeads() {
 }
 */
 
+/*
 //S5L39 Checking localStorage before rendering
 
 let myLeads = []
@@ -385,6 +386,55 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads();
+
+    console.log(localStorage.getItem("myLeads"));
+    
+})
+
+function renderLeads() {
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li> 
+            <a href= '${myLeads[i]}' +  target = '_blank'> ${myLeads[i]} </a>
+        </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
+*/
+
+//S5L41 Make the delete button work
+
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()   
+})
 
 
 inputBtn.addEventListener("click", function(){
