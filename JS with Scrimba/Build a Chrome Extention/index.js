2edd2147cd1d0da3e4b10d8c25f3c3b8899b2861
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-13 21:56:36 +0600

    Enhancement #15: Create save tab button

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -464,8 +464,8 @@ function renderLeads() {
 }
 */
 
+/*
 //S5L48 Refactor renderLeads() to use a parameter
-
 
 let myLeads = []
 let inputEl = document.getElementById("input-el")
@@ -484,6 +484,64 @@ if (leadsFromLocalStorage) {
 // Refector the function so that it takes a parameter, leads, that it uses
 // instead of the global myLeads variable. Remember to update all invocations 
 // of the function as well.
+
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li> 
+            <a href= '${leads[i]}' +  target = '_blank'> ${leads[i]} </a>
+        </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)   
+})
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+
+    console.log(localStorage.getItem("myLeads"));
+    
+})
+*/
+
+//S5L49 Create the tabBtn
+
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+tabBtn.addEventListener("click", function(){
+    console.log(tabs[0].url);
+})
 
 function render(leads) {
     let listItems = ""
