7cc655b17839d8103dbf4f3c5113cd50fe451232
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-13 19:28:56 +0600

    Enhancement #15: Writing first function parameter

--- JS with Scrimba/Build a Chrome Extention/Detour/index.js
+++ JS with Scrimba/Build a Chrome Extention/Detour/index.js
@@ -1,43 +1,12 @@
-/*
-//S5L25 Write your first template string
 
-// template strings/literals
+//S5L43 Write your first function parameter
 
-const recipient = "James"
+const welcomeEl = document.getElementById("welcome-el")
 
-// Refactor the email string to use tamplate strings
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+// Give the function a parameter, greeting, that replaces "Welcome back"
 
-console.log(email)
-*/
+function greetUser(greeting) {
+    welcomeEl.textContent = greeting + "Per Harald Borgen"
+}
 
-/*
-//S5L26 Make the template string even more dynamic
-
-// template strings/literals
-
-const recipient = "James"
-const sender = "Per"
-
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
-
-console.log(email)
-*/
-
-//S5L27 Template strings on multiple lines
-
-// template strings/literals
-
-const recipient = "James"
-const sender = "Per"
-
-const email = `
-Hey ${recipient}! 
-
-How is it going?
-
-Cheers ${sender}
-`
-
-console.log(email)
-
+greetUser("Welcome back, ")
