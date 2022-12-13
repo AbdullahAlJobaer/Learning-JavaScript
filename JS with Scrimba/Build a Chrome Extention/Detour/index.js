1d8448dcb49e1e5bb0089dfa9381a5a898ec4d2f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-13 19:45:11 +0600

    Enhancement #15: Numbers as functions parameters

--- JS with Scrimba/Build a Chrome Extention/Detour/index.js
+++ JS with Scrimba/Build a Chrome Extention/Detour/index.js
@@ -1,4 +1,5 @@
 
+/*
 //S5L44 Functions with multiple parameters
 
 const welcomeEl = document.getElementById("welcome-el")
@@ -10,3 +11,15 @@ function greetUser(greeting, name) {
 }
 
 greetUser("Welcome back", "Per Harald Borgen!")
+*/
+
+//S5L45 Numbers as functions parameters
+
+// Create a function, add(), that adds two numbers together and returns the sum
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+console.log( add(3, 4) );
+console.log( add(9, 102) );
