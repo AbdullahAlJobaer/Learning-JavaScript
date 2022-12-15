70a6ea374cd3a9d3094c34ef6db0e4c478fab3dd
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-16 03:29:03 +0600

    Enhancement #15: Recap
    Concepts I've learned through the building of this chrome extention.

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -691,3 +691,19 @@ inputBtn.addEventListener("click", function(){
 
     console.log(localStorage.getItem("myLeads"));    
 })
+
+
+/*
+
+    RECAP
+    - const
+    - addEventListener()
+    - innerHTML
+    - input.value
+    - function parameters
+    - template strings
+    - localStorage
+    - The JSON object
+    - Objects in arrays
+    
+*/
