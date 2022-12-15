5e0fdd97121bbff660b8359779685f9d0f0ece6f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-15 14:12:27 +0600

    Enhancement #15: Saving the tab url

--- JS with Scrimba/Build a Chrome Extention/index.js
+++ JS with Scrimba/Build a Chrome Extention/index.js
@@ -517,7 +517,7 @@ inputBtn.addEventListener("click", function(){
     
 })
 */
-
+/*
 //S5L49 Create the tabBtn
 
 let myLeads = []
@@ -541,6 +541,69 @@ const tabs = [
 // 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
 tabBtn.addEventListener("click", function(){
     console.log(tabs[0].url);
+})
+
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li> 
+            <a href= '${leads[i]}' +  target = '_blank'> ${leads[i]} </a>
+        </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)   
+})
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+
+    console.log(localStorage.getItem("myLeads"));
+    
+})
+*/
+
+
+//S5L50 Save the tab url
+
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+tabBtn.addEventListener("click", function(){
+    // Save the url instead of logging it out
+    console.log(tabs[0].url);
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
 function render(leads) {
