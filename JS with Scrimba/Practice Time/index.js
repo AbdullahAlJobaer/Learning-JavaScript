70dfa80342a2e8f9fc35af8aabf64077ca308177
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-12-17 23:34:06 +0600

    Enhancement #13: Log out items in an array

--- JS with Scrimba/Practice Time/index.js
+++ JS with Scrimba/Practice Time/index.js
@@ -110,9 +110,6 @@ if (dayOfMonth === 13 && weekday === "Friday") {
     console.log("😱");
 }
 
-*/
-
-
 //S4L6 Rock papers scissors
 
 let hands = ["Rock", "Paper", "Scissor"]
@@ -123,3 +120,44 @@ function getHand() {
 }
 
 console.log(getHand());
+*/
+/*
+//S6L1 let and const
+
+// SETTING THE STAGE
+const player = "Per"
+const opponent = "Nick"
+const game = "AmazingFighter"
+let points = 0
+let hasWon = false
+
+// PLAYING THE GAME
+points += 100
+hasWon = true
+
+// ANNOUNCING THE WINNER
+if (hasWon) {
+    console.log(`${player} got ${points} points and won the ${game} game!`)
+} else {
+    console.log(`The winner is ${opponent}! ${player} lost the game`)
+}
+
+// Go through all variables and decide if they should be let or const
+// Change the console logs to use template strings instead of double quotes
+*/
+
+//S6L2 Log out items in an array
+
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+// Create a function that takes a single parameter, an array,
+// and logs all the items of the array to the console.
+// Call the function while passing in myCourses as an argument
+
+function showCourse(arr) {
+    for (let i = 0; i<arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+showCourse(myCourses)
