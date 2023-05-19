35ab68197a857eabf3155fc591b9131d737b762f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-05-20 00:08:09 +0600

    Assignment - Data types (12)

--- JS with Jonas Schmedtmann/01-Fundamentals-Part-1/starter/assignments.js
+++ JS with Jonas Schmedtmann/01-Fundamentals-Part-1/starter/assignments.js
@@ -10,3 +10,16 @@ console.log(continent);
 console.log(population);
 */
 
+// Assignment - Data types (12)
+
+let country = "Bangladesh";
+let continent = "South Asia";
+let population = 169.4;
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
