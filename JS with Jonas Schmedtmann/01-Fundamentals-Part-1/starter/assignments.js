4db0570c7bd6e148450ec8c880c2b3312a696159
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-05-29 14:23:30 +0600

    NEW Coding challenge 3 (25)

--- JS with Jonas Schmedtmann/01-Fundamentals-Part-1/starter/assignments.js
+++ JS with Jonas Schmedtmann/01-Fundamentals-Part-1/starter/assignments.js
@@ -93,7 +93,6 @@ console.log("19" - "13" + "17");        // 617
 console.log("19" - "13" + 17);          // 23
 console.log("123" < 57);                // false
 console.log(5 + 6 + "4" + 9 - 4 - 2);   // 1143
-*/
 
 
 
@@ -108,3 +107,4 @@ if (language === "English" && population < 50 && isIsland === true) {
 } else {
     console.log(`${country} does not meet Sara's criteria`);
 }
+*/
