087d1d233a99a23fd692ccaadd1dd7393f3f36ce
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-05-31 00:18:17 +0600

    Assignment - Functions (33)

---
+++ JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Assignment - Functions (33)
+
+function describeCountry(country, population, capitalCity) {
+    const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return description;
+}
+
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+const descSpain = describeCountry("Spain", 47, "Madrid");
+const descBangladesh = describeCountry("Bangladesh", 170, "Dhaka");
+
+console.log(descFinland);
+console.log(descSpain);
+console.log(descBangladesh);
