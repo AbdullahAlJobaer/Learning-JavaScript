639b465b3dbae124c51b36227bb4fc9d242695db
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-05-30 18:01:54 +0600

    Activating strict mode (32)

---
+++ JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+// 32 - Activating strict mode
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio";
+// const private = "7001";
+// const if = "how";
