8b432c5ce05c41c31d5bf1bb6c00c9bb8590d1b0
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-05-31 00:05:19 +0600

    Functions (33)

--- JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/script.js
+++ JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // 32 - Activating strict mode
 
 "use strict";
@@ -11,3 +12,26 @@ if (hasDriversLicense) console.log("I can drive");
 // const interface = "Audio";
 // const private = "7001";
 // const if = "how";
+*/
+
+
+
+// 33 - Functions
+
+function logger() {
+    console.log("My name is Robin");
+}
+
+logger();   // Calling, running, or invoking function
+
+
+function fruitProcessor(oranges, apples) {
+    const juice = `Juice made with ${oranges} oranges and ${apples} apples`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(0, 2);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
