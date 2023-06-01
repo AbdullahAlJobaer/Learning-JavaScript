48e92f1d48f758f6b7bd4bafb01cea13021f7437
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-02 00:05:52 +0600

    Coding Challenge 1 (38)

--- JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/script.js
+++ JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/script.js
@@ -101,7 +101,7 @@ function fruitProcessor(oranges, apples) {
 
 const finalJuice = fruitProcessor(2, 3);
 console.log(finalJuice);
-*/
+
 
 
 
@@ -127,3 +127,60 @@ const retiresRobin = yearsUntilRetirement(1997, "Robin");
 console.log(retiresRobin);
 const retiresRite = yearsUntilRetirement(1955, "Rite");
 console.log(retiresRite);
+*/
+
+
+
+//////////////////////////////////////////
+/// 38 - CODING CHALLENGE #1 ////////////
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+
+YOUR TASKS:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+
+2. Use the function to calculate the average for both teams
+
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+
+5. Ignore draws this time
+
+TEST DATA:
+§ DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+*/
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const dolphinsFinal = calcAverage(85, 54, 41);
+const KoalasFinal = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolphins, avgKoalas) {
+
+    if (avgDolphins >= avgKoalas * 2) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+    } else if (avgKoalas >= avgDolphins * 2) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+    } else {
+        return `No team wins`;
+    }
+}
+
+console.log(checkWinner(dolphinsFinal, KoalasFinal));
+
+
+// dolphinResult > koalasResult * 2
+
+
