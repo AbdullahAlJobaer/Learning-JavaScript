1cfadbf7106acd60e7dd4d211e189461e7e68243
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-06 17:22:44 +0600

    Coding challenge 3 (CE7)

--- JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/assignment.js
+++ JS with Jonas Schmedtmann/02-Fundamentals-Part-2/starter/assignment.js
@@ -156,22 +156,11 @@ console.log(myCountry.population);
 
 myCountry["population"] -= 2;
 console.log(myCountry["population"]);
-*/
+
 
 
 
 // Assignment - Object methods (38)
-
-/*
-1. Add a method called 'describe' to the 'myCountry' object. This method
-will log a string to the console, similar to the string logged in the previous
-assignment, but this time using the 'this' keyword.
-2. Call the 'describe' method
-3. Add a method called 'checkIsland' to the 'myCountry' object. This
-method will set a new property on the object, called 'isIsland'.
-'isIsland' will be true if there are no neighbouring countries, and false if
-there are. Use the ternary operator to set the property.
-*/
 
 const myCountry = {
     country: "Bangladesh",
@@ -193,3 +182,4 @@ const myCountry = {
 myCountry.describe();
 console.log(myCountry.checkIsland());
 //console.log(myCountry.isIsland);
+*/
