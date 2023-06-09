3572310e3fc0e19c90fd6b990057d4186f37259b
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-09 23:04:42 +0600

    Assignment - Equality Operators: == vs. === (22)

--- JS with Jonas Schmedtmann/Sec 02 - Fundamentals - Part 1/starter/assignments.js
+++ JS with Jonas Schmedtmann/Sec 02 - Fundamentals - Part 1/starter/assignments.js
@@ -1,6 +1,11 @@
 /*
 Assignment - Values and variables (10)
 
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+
+
 let country = "Bangladesh";
 let continent = "South Asia";
 let population = 170;
@@ -12,6 +17,12 @@ console.log(population);
 
 
 // Assignment - Data types (12)
+
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
 
 let country = "Bangladesh";
 let continent = "South Asia";
@@ -29,6 +40,13 @@ console.log(typeof language);
 
 // Assignment - let, const, and var (13)
 
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+
+
 const country = "Bangladesh";
 const continent = "Asia";
 let population = 170;
@@ -41,6 +59,18 @@ const isIsland = true;
 
 
 // Assignment - Basic operators (14)
+
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+
 
 let country = "Bangladesh";
 const continent = "Asia";
@@ -61,6 +91,10 @@ console.log(description);
 
 // Assignment - Strings and template literals (17)
 
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
+
+
 let country = "Bangladesh";
 const continent = "Asia";
 const language = "Bengali";
@@ -72,6 +106,14 @@ console.log(description);
 
 
 // Assignment - Taking decisions: if else statements (18)
+
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+
 
 const country = "Bangladesh";
 const population = 170;
@@ -88,6 +130,16 @@ console.log(description);
 
 
 // Assignment - Type conversion and coercion (20)
+
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+2. Execute the operations to check if you were right
+
+
 console.log("9" - "5");                 // 4
 console.log("19" - "13" + "17");        // 617
 console.log("19" - "13" + 17);          // 23
@@ -97,6 +149,20 @@ console.log(5 + 6 + "4" + 9 - 4 - 2);   // 1143
 
 
 // Assignment - Logical operators (24)
+
+1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island.
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in Canada :D)
+
+
 const country = "Bangladesh";
 const language = "Bengali";
 const population = 170;
@@ -111,6 +177,15 @@ if (language === "English" && population < 50 && isIsland === true) {
 
 
 // Assignment - The switch statement (26)
+
+1. Use a switch statement to log the following string for the given 'language':
+chinese or mandarin: 'MOST number of native speakers!'
+spanish: '2nd place in number of native speakers'
+english: '3rd place'
+hindi: 'Number 4'
+arabic: '5th most spoken language'
+for all other simply log 'Great language too :D'
+
 
 const language = prompt("Which language?");
 
@@ -140,9 +215,45 @@ switch (language) {
 
 // Assignment - The conditional (ternary) operator (28)
 
+1. If your country's population is greater than 33 million, use the ternary operator
+to log a string like this to the console: 'Portugal's population is above average'.
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+
+
 const country = "Bangladesh";
 const population = 170;
 const str = `${country}'s population is ${population > 33 ? "above" : "below"} average`;
 
 console.log(str);
 */
+
+
+// Assignment - Equality Operators: == vs. === (22)
+
+/*
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality == for now)
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours' is greater than 1
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why is this happening?
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now when you input 1
+8. Reflect on why we should use the === operator and type conversion in this situation
+*/
+
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeighbours === 1) {
+    console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+    console.log("More than 1 border");
+} else {
+    console.log("No borders!");
+}
