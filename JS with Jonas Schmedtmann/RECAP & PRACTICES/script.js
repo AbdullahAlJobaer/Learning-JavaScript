596013ac5d06c07bf36b5838632f75719407bc82
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-12 16:22:38 +0600

    Coding exercise 2 RECAP

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
@@ -19,7 +19,7 @@ BONUS: Create a boolean variable markHigherBMI containing information about whet
 higher BMI than John. Log it to the console too
 
 TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
-*/
+
 
 const massMark = 78;
 const heightMark = 1.69;
@@ -34,3 +34,36 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark);
 console.log(BMIJohn);
 console.log(markHigherBMI);
+*/
+
+
+
+/*
+CHALLENGE #2 (CE #2)
+
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is 
+higher than Mark's (27)!".
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+}
