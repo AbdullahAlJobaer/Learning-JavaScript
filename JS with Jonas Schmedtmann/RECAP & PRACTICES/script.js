0af6fdde050a358818d506fbcf0433e92736a7d6
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-15 02:02:05 +0600

    Coding exercise 4 RECAP done

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
@@ -73,31 +73,6 @@ if (BMIMark > BMIJohn) {
 /*
 CHALLENGE #3 (CE #3)
 
-There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. 
-The winner with the highest average score wins a trophy!
-
-YOUR TASKS:
-
-1. Calculate the average score for each team, using the test data included below. 
-The average score for Dolphins should be assigned to the scoreDolphins variable, and the average 
-score of Koalas should be assigned to the scoreKoalas variable.
-
-2. Compare the team's average scores to determine the winner of the competition, and print to the console:
-
-"Dolphins win the trophy" if Dolphins win, or
-
-"Koalas win the trophy" if Koalas win, or
-
-"Both win the trophy" if their average scores are equal.
-
-TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
-*/
-
-
-
-/*
-CHALLENGE #3
-
 There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times.
 The winner with the highest average score wins a trophy!
 
@@ -139,18 +114,18 @@ if (scoreDolphins > scoreKoalas) {
 
 
 /*
-CHALLENGE #4
+CHALLENGE #4 (CE #4)
 
 Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, 
 it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 YOUR TASKS:
 
-Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed 
+1. Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed 
 to use an if...else statement (if it's easier for you, you can start with an if...else statement, 
 and then try to convert it to a ternary operator).
 
-Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
 
 Example: The bill was 275, the tip was 41.25, and the total value 316.25.
 
@@ -158,3 +133,9 @@ Note: Use the values of the bill and tip variables to construct this string. Don
 
 TEST DATA: Test with different bill values: 275, 40, and 430
 */
+
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const final = tip + bill;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${final}`);
