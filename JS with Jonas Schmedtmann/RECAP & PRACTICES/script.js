2f1156d60af6964a30f9bbdaad275c17510e60f4
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-15 17:39:54 +0600

    Coding exercise 5 RECAP done

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
@@ -174,3 +174,19 @@ TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
 TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
 */
 
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins!");
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
