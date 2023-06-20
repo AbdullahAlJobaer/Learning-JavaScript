817e3144b793d9718230ede71c305e0ab89e416f
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-20 18:21:51 +0600

    Update script.js

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/script.js
@@ -3,19 +3,19 @@
 /*
 CODING EXERCISE 1
 
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using 
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using
 the formula: BMI = mass / (height * height) (mass in kg and height in meters).
 
 Your task is to write some code to help them:
 
 1. Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
 
-2. Calculate both their BMIs using the formula, and store the results in two variables called BMIMark 
+2. Calculate both their BMIs using the formula, and store the results in two variables called BMIMark
 and BMIJohn.
 
 3. Log the value of BMIMark and BMIJohn to the console.
 
-BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a 
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a
 higher BMI than John. Log it to the console too
 
 TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
@@ -49,7 +49,7 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 
 2. Modify the outputs above to use template literals to include the BMI values in the outputs.
 
-Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is 
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is
 higher than Mark's (27)!".
 
 const massMark = 78;
@@ -78,8 +78,8 @@ The winner with the highest average score wins a trophy!
 
 Your tasks:
 
-1. Calculate the average score for each team, using the test data included below. The average score 
-for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas 
+1. Calculate the average score for each team, using the test data included below. The average score
+for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas
 should be assigned to the scoreKoalas variable.
 
 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
@@ -116,13 +116,13 @@ if (scoreDolphins > scoreKoalas) {
 /*
 CODING EXERCISE 4
 
-Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, 
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country,
 it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 YOUR TASKS:
 
-1. Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed 
-to use an if...else statement (if it's easier for you, you can start with an if...else statement, 
+1. Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed
+to use an if...else statement (if it's easier for you, you can start with an if...else statement,
 and then try to convert it to a ternary operator).
 
 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
@@ -146,24 +146,24 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${fin
 /*
 CODING EXERCISE #5
 
-Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, 
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline,
 which works differently.
 
-Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score 
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score
 per team).
 
 A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
 
 YOUR TASKS:
-1. Create an arrow function calcAverage to calculate the average of 3 scores. This function should have 
+1. Create an arrow function calcAverage to calculate the average of 3 scores. This function should have
 three parameters and return a single number (the average score).
 
-2. Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the 
+2. Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the
 calcAverage function to them (you will need to call this function, and pass scores as arguments).
 
-3. Create a function checkWinner that takes the average score of each team as parameters 
-(avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, 
-according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead 
+3. Create a function checkWinner that takes the average score of each team as parameters
+(avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points,
+according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead
 of hard-coded values).
 
 4. Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
