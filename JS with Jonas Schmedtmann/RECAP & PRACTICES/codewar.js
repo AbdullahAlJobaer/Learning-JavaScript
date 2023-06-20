e5db6d3cc4b4cead2ecf1084e2e9828bc13fb65c
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-20 20:25:16 +0600

    Convert number to reversed array of digits DONE

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
@@ -5,7 +5,6 @@
 //////////////////////////////////////////////////
 
 /*
-
 CHALLENGE: Square(n) Sum
 
 Complete the square sum function so that it squares each number passed into it and then sums the 
@@ -13,7 +12,7 @@ results together.
 
 For example, for [1, 2, 2] it should return 9
 */
-
+/*
 function squareSum(numbers) {
     let sumOfSqares = 0;
     for (let i = 0; i < numbers.length; i++) {
@@ -21,3 +20,35 @@ function squareSum(numbers) {
     } return sumOfSqares;
 }
 console.log(squareSum([1, 2, 2]));
+*/
+
+
+/*
+CHALLENGE: Convert number to reversed array of digits
+
+Convert number to reversed array of digits.
+Given a random non-negative number, you have to return the digits of this number within an array 
+in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+*/
+
+// Sub-problems:
+// 1) Transform the numbers into an array
+// 1) Reverse the number using for loop
+// 2) Push the reversed numbers in an empty array
+
+function digitize(n) {
+    let number = Array.from(String(n), Number);
+    let result = [];
+
+    for (let i = number.length - 1; i >= 0; i--) {
+        result.push(number[i]);
+    }
+
+    return result;
+}
+
+console.log(digitize(321));
