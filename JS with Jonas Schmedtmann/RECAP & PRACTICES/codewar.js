826f2eb403902984ba6c04001038c3ce8c38f383
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-24 16:43:24 +0600

    CHALLENGE: Reduce but Grow Done

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
@@ -90,3 +90,24 @@ function stringToArray(string) {
 
 console.log(stringToArray("I love arrays they are my favorite"));
 */
+
+
+
+
+/*
+CHALLENGE: Beginner - Reduce but Grow
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. 
+Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+function grow(x) {
+    let result = 1;
+
+    for (let i = 0; i < x.length; i++) {
+        result *= x[i];
+    } return result;
+}
+
+const final = grow([1, 2, 3, 4]);
+console.log(final);
