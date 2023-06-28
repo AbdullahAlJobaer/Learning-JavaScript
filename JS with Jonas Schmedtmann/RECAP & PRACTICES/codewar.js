32ec3155e75bb100fcc0b7cc8c460d354e57fd43
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-28 18:12:18 +0600

    Challenge: Rock Paper Scissors, 2nd Solution

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
@@ -237,23 +237,40 @@ Examples(Input1, Input2 --> Output):
 "paper", "paper" --> "Draw!"
 */
 
+// const rps = (p1, p2) => {
+//     if (p1 === "scissors" && p2 === "paper") {
+//         return "Player 1 won!";
+//     } else if (p1 === "paper" && p2 === "scissors") {
+//         return "Player 2 won!"
+//     } else if (p1 === "scissors" && p2 === "rock") {
+//         return "Player 2 won!";
+//     } else if (p1 === "rock" && p2 === "scissors") {
+//         return "Player 1 won!";
+//     } else if (p1 === "paper" && p2 === "rock") {
+//         return "Player 1 won!";
+//     } else if (p1 === "rock" && p2 === "paper") {
+//         return "Player 2 won!"
+//     } else if (p1 === p2) {
+//         return "Draw!";
+//     }
+// };
+
 const rps = (p1, p2) => {
-    if (p1 === "scissors" && p2 === "paper") {
+    if (p1 === p2) return "Draw!";
+    var rules = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+
+    if (p2 === rules[p1]) {
         return "Player 1 won!";
-    } else if (p1 === "paper" && p2 === "scissors") {
-        return "Player 2 won!"
-    } else if (p1 === "scissors" && p2 === "rock") {
+    }
+    else {
         return "Player 2 won!";
-    } else if (p1 === "rock" && p2 === "scissors") {
-        return "Player 1 won!";
-    } else if (p1 === "paper" && p2 === "rock") {
-        return "Player 1 won!";
-    } else if (p1 === "rock" && p2 === "paper") {
-        return "Player 2 won!"
-    } else if ((p1 === "paper" && p2 === "paper") || (p1 === "rock" && p2 === "rock") || ((p1 === "scissors" && p2 === "scissors"))) {
-        return "Draw!";
     }
 };
 
 const result = rps("rock", "paper");
 console.log(result);
+
