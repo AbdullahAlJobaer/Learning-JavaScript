3ad534d4c78af943b8c5158b5fb519593a1b3cfc
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-06-29 19:50:46 +0600

    Challenge: L1 - Set Alarm Starts

--- JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
+++ JS with Jonas Schmedtmann/RECAP & PRACTICES/codewar.js
@@ -236,7 +236,7 @@ Examples(Input1, Input2 --> Output):
 "scissors", "rock" --> "Player 2 won!"
 "paper", "paper" --> "Draw!"
 */
-
+/*
 // const rps = (p1, p2) => {
 //     if (p1 === "scissors" && p2 === "paper") {
 //         return "Player 1 won!";
@@ -273,3 +273,23 @@ const rps = (p1, p2) => {
 
 const result = rps("rock", "paper");
 console.log(result);
+*/
+
+
+/*
+CHALLENGE: L1: Set Alarm
+
+Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives 
+two parameters. The first parameter, employed, is true whenever you are employed and the second 
+parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the 
+circumstances under which you need to set an alarm). It should return false otherwise. 
+
+Examples:
+employed | vacation 
+true     | true     => false
+true     | false    => true
+false    | true     => false
+false    | false    => false
+*/
