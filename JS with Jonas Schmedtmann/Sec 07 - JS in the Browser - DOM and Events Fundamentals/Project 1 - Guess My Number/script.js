4fd2b88c5e05517a263fe2d2a18d74a23c50fb06
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-07-03 14:19:25 +0600

    Selecting and Manipulating Elements (72)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
@@ -4,3 +4,19 @@
 /*
 console.log(document.querySelector(".message").textContent);
 */
+
+
+
+// 72 - Selecting and Manipulating Elements
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "😎 Correct Number!";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 10;
+document.querySelector(".score").textContent = 5;
+
+document.querySelector(".guess").value = 10;
+console.log(document.querySelector(".guess").value);
