2f8f57a16cf8c2b39029e071d5ee61966cce889b
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-07-04 15:20:31 +0600

    Handling Click Events (73)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
@@ -8,7 +8,7 @@ console.log(document.querySelector(".message").textContent);
 
 
 // 72 - Selecting and Manipulating Elements
-
+/*
 console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "😎 Correct Number!";
@@ -20,3 +20,19 @@ document.querySelector(".score").textContent = 5;
 
 document.querySelector(".guess").value = 10;
 console.log(document.querySelector(".guess").value);
+*/
+
+
+
+// 73 - Handling Click Events
+
+document.querySelector(".check").addEventListener
+    ("click", function () {
+        const guess = Number(document.querySelector(".guess").value)
+
+        console.log(guess, typeof guess);
+
+        if (!guess) {
+            document.querySelector(".message").textContent = "No Number ⛔";
+        }
+    });
