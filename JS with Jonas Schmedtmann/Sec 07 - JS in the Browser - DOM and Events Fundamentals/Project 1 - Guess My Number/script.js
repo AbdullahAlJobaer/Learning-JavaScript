599d3f125360016a0b94a569e62fd189dbe7b1ee
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-07-06 01:49:25 +0600

    Implementing Highscores (77)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
@@ -149,7 +149,7 @@ fields
 4. Also restore the original background color (#222) and number width (15rem)
 */
 
-
+/*
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;     // Application state
 
@@ -170,6 +170,77 @@ document.querySelector(".check").addEventListener
             document.querySelector("body").style.backgroundColor = "#60b347";
             document.querySelector(".number").style.width = "30rem";
             document.querySelector(".number").style.transition = "width 500ms";
+
+            // When guess is too high
+        } else if (guess > secretNumber) {
+            if (score > 1) {
+                document.querySelector(".message").textContent = "📈 Too High!";
+                score--;
+                document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = "❌ You Lost the Game!";
+                document.querySelector(".score").textContent = 0;
+            }
+
+            // When guess is too low
+        } else if (guess < secretNumber) {
+            if (score > 1) {
+                document.querySelector(".message").textContent = "📉 Too Low!";
+                score--;
+                document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = "❌ You Lost the Game!";
+                document.querySelector(".score").textContent = 0;
+            }
+        }
+    });
+
+document.querySelector(".again").addEventListener
+    ("click", function () {
+        score = 20;
+        secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+        document.querySelector(".score").textContent = score;
+        document.querySelector(".number").textContent = "?";
+        document.querySelector("body").style.backgroundColor = "#222";
+        document.querySelector(".number").style.width = "15rem";
+        document.querySelector("body").style.transition = "background-color 400ms, width 400ms";
+        document.querySelector(".message").textContent = "Start guessing...";
+        document.querySelector(".guess").value = null;
+    });
+*/
+
+
+
+// 77 - Implementing Highscores
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;     // Application state
+let highScore = 0;
+
+document.querySelector(".check").addEventListener
+    ("click", function () {
+        const guess = Number(document.querySelector(".guess").value)
+
+        console.log(guess, typeof guess);
+
+        // When there is no input
+        if (!guess) {
+            document.querySelector(".message").textContent = "⛔ No Number";
+
+            // When player wins
+        } else if (guess === secretNumber) {
+            document.querySelector(".number").textContent = secretNumber;
+            document.querySelector(".message").textContent = "🎉 Correctly Guessed!";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+            document.querySelector(".number").style.transition = "width 500ms";
+
+            if (score > highScore) {
+                highScore = score;
+                document.querySelector(".highscore").textContent = highScore;
+            }
+
 
             // When guess is too high
         } else if (guess > secretNumber) {
