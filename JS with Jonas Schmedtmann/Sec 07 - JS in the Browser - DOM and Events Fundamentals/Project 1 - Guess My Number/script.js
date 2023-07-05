d1586687ac9b1191f07014a4bdc8c1fbbf674636
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-07-05 16:49:11 +0600

    Manipulating CSS Styles (75)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 1 - Guess My Number/script.js
@@ -41,7 +41,7 @@ document.querySelector(".check").addEventListener
 
 
 // 74 - Implementing the Game Logic
-
+/*
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;     // Application state
 
@@ -67,6 +67,58 @@ document.querySelector(".check").addEventListener
                 document.querySelector(".message").textContent = "❌ You Lost the Game!";
                 document.querySelector(".score").textContent = 0;
             }
+        } else if (guess < secretNumber) {
+            if (score > 1) {
+                document.querySelector(".message").textContent = "📉 Too Low!";
+                score--;
+                document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = "❌ You Lost the Game!";
+                document.querySelector(".score").textContent = 0;
+            }
+        }
+    });
+*/
+
+
+
+// 75 - Manipulating CSS Styles
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;     // Application state
+
+document.querySelector(".number").textContent = secretNumber;
+
+
+document.querySelector(".check").addEventListener
+    ("click", function () {
+        const guess = Number(document.querySelector(".guess").value)
+
+        console.log(guess, typeof guess);
+
+        // When there is no input
+        if (!guess) {
+            document.querySelector(".message").textContent = "⛔ No Number";
+
+            // When player wins
+        } else if (guess === secretNumber) {
+            document.querySelector(".message").textContent = "🎉 Correctly Guessed!";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+            document.querySelector(".number").style.transition = "width 500ms";
+
+            // When guess is too high
+        } else if (guess > secretNumber) {
+            if (score > 1) {
+                document.querySelector(".message").textContent = "📈 Too High!";
+                score--;
+                document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = "❌ You Lost the Game!";
+                document.querySelector(".score").textContent = 0;
+            }
+
+            // When guess is too low
         } else if (guess < secretNumber) {
             if (score > 1) {
                 document.querySelector(".message").textContent = "📉 Too Low!";
