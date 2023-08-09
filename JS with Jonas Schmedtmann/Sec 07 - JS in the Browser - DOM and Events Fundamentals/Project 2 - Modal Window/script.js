9c7115f419f7770dbf20d674ab55c3240a7c0e25
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-08-09 22:41:23 +0600

    Working with classes (80)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 2 - Modal Window/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 2 - Modal Window/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // 79 - Project: Modal window
-
+/*
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
@@ -12,3 +12,32 @@ console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++) {
     console.log(btnsOpenModal[i].textContent);
 }
+*/
+
+
+
+// 80 - Working with classes
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
