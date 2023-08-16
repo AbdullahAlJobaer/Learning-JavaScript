4828db41204b9729605484669140db662668e2e3
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-08-16 14:15:03 +0600

    Handling an "Esc" Keypress Event (81)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 2 - Modal Window/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 2 - Modal Window/script.js
@@ -17,6 +17,35 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 
 // 80 - Working with classes
+/*
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+*/
+
+
+
+// 81 - Handling an "Esc" Keypress Event
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
@@ -41,3 +70,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
