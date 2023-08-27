22eeb9339ac8aaeeec9416720f2fa7028ea50b7e
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2023-08-27 15:48:03 +0600

    Rolling the Dice (83)

--- JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 3 - Pig Game/script.js
+++ JS with Jonas Schmedtmann/Sec 07 - JS in the Browser - DOM and Events Fundamentals/Project 3 - Pig Game/script.js
@@ -20,21 +20,37 @@ diceEl.classList.add("hidden");
 // Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector("btn--roll");
+const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 // Rolling dice functionality
-btnHold.addEventListener("click", function () {
+btnRoll.addEventListener("click", function () {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
+
     // 2. Display dice roll
     diceEl.classList.remove("hidden");
-    // 3. Check for rolled 1: if true, switch to next player
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled
+    if (dice !== 1) {
+        // Add dice roll to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE LATER
+    } else {
+        // Switch to next player
+
+    }
 });
